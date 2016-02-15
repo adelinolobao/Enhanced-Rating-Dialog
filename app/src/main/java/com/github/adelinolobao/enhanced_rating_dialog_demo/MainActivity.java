9897146f06339ab8d8f9278dbe97b6c1ecfcc133
Adelino Lobao <adelino.lobao@boostermedia.com>
2016-02-15 13:43:27 +0100

    added base implementation for rating popup

--- app/src/main/java/com/github/adelinolobao/enhanced_rating_dialog_demo/MainActivity.java
+++ app/src/main/java/com/github/adelinolobao/enhanced_rating_dialog_demo/MainActivity.java
@@ -3,10 +3,14 @@ package com.github.adelinolobao.enhanced_rating_dialog_demo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.adelinolobao.enhanced_rating_dialog.RatingDialog;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        RatingDialog.show(this);
     }
 }
